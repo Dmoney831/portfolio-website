8408fddcffc1647b78b57f85306f6a2a1703bff8
Derek Lee <dyklee89@gmail.com>
2022-03-29 01:18:03 -0400

    text overlay

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,9 +7,9 @@ import Triton from '../images/triton.png'
 import Show_Us from '../images/show_us.png'
 import BJ from '../images/BJ.png'
 import PP from '../images/PP.png'
-import easyPeasy from'../images/easyPeasy.png'
+import easyPeasy from '../images/easyPeasy.png'
 import './style/Projects.css'
-import {list_of_projects} from '../data/Data.jsx'
+import { list_of_projects } from '../data/Data.jsx'
 
 
 function Projects() {
@@ -21,32 +21,126 @@ function Projects() {
         )
     })
     return (
-        <section className='mb-5'>
-            <div className='proj0'>
-                <div className='proj1'>
-                    <h4>Projects</h4>
-                    <h5>List of Creations</h5>
-                    {projectsObject}
-                </div>
-                <div className='proj-cards'>
-                    <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
-                        <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={Triton} />
-                    </Card>
-                    <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
-                        <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={Show_Us} />
-                    </Card>
-                    <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
-                        <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={BJ} />
-                    </Card>
-                    <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
-                        <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={easyPeasy} />
-                    </Card>
-                    <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
-                        <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={PP} />
-                    </Card>
+        <div className="section-header text-center  mt-5">
+            <h4>projects</h4>
+            <p>list of proejcts</p>
+
+            <div className="container proj-container">
+                <div className="row justify-content-center">
+                    <div className="col-md-5 col-sm-6 my-5">
+                        <div className="proj-card card">
+                            <img className='proj-img' src={Triton} />
+                            <div className="proj-text card-text">
+                                <h3 className='title'>Title</h3>
+                                <h5 className='description'>Description</h5>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
-        </section>
+        </div>
+        // <section className='mb-5'>
+        //     <div className='proj0'>
+        //         <div className='proj1'>
+        //             <h4>Projects</h4>
+        //             <h5>List of Creations</h5>
+        //             {projectsObject}
+        //         </div>
+        //         <div className='proj-cards'>
+        //             <div>
+        //                     <div className='card-text'>
+        //                         <h5>title</h5>
+        //                         <h6>description</h6>
+        //                     </div>
+        //                 <Card className='proj-card mx-5 my-3' style={{ width:310, height: 410}}>
+        //                     <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={Triton} />
+        //                 </Card>
+        //             </div>
+
+        //             <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
+        //                 <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={Show_Us} />
+        //             </Card>
+        //             <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
+        //                 <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={BJ} />
+        //             </Card>
+        //             <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
+        //                 <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={easyPeasy} />
+        //             </Card>
+        //             <Card className='proj-card mx-5 my-3' style={{ width:310, height: 310}}>
+        //                 <Card.Img className='proj-img mx-auto my-auto' style={{ width: 300, height: 300 }} src={PP} />
+        //             </Card>
+        //         </div>
+
+        //     </div>
+        //         <div className="container">
+        //             <div className="row">
+        //                 <div className="col-md-12">
+        //                     <div className="section-header text-center">
+        //                         <h4>projects</h4>
+        //                         <p>list of proejcts</p>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //             <div className="row">
+        //                 <div className="col-md-4 col-sm-6 col-xs-12">
+        //                     <div className="single-team">
+        //                         <div className="team-img">
+        //                             <img className='proj-img'  src={Triton} alt=""/>
+        //                         </div>    
+        //                     </div>
+        //                     <div className="overlay-text">
+        //                         <h2>title</h2>
+        //                         <p>description</p>
+        //                     </div>
+        //                 </div> 
+        //                 <div className="col-md-4 col-sm-6 col-xs-12">
+        //                     <div className="single-team">
+        //                         <div className="team-img">
+        //                             <img className='proj-img'  src={Sample} alt=""/>
+        //                         </div>    
+        //                     </div>
+        //                     <div className="overlay-text">
+        //                         <h4>title</h4>
+        //                         <p>description</p>
+        //                     </div>
+        //                 </div> 
+        //                 <div className="col-md-4 col-sm-6 col-xs-12">
+        //                     <div className="single-team">
+        //                         <div className="team-img">
+        //                             <img className='proj-img' style={{ width:310, height: 310}} src={Sample} alt=""/>
+        //                         </div>    
+        //                     </div>
+        //                     <div className="overlay-text">
+        //                         <h4>title</h4>
+        //                         <p>description</p>
+        //                     </div>
+        //                 </div> 
+        //                 <div className="col-md-4 col-sm-6 col-xs-12">
+        //                     <div className="single-team">
+        //                         <div className="team-img">
+        //                             <img className='proj-img' style={{ width:310, height: 310}} src={Sample} alt=""/>
+        //                         </div>    
+        //                     </div>
+        //                     <div className="overlay-text">
+        //                         <h4>title</h4>
+        //                         <p>description</p>
+        //                     </div>
+        //                 </div> 
+        //                 <div className="col-md-4 col-sm-6 col-xs-12">
+        //                     <div className="single-team">
+        //                         <div className="team-img">
+        //                             <img className='proj-img' style={{ width:310, height: 310}} src={Sample} alt=""/>
+        //                         </div>    
+        //                     </div>
+        //                     <div className="overlay-text">
+        //                         <h4>title</h4>
+        //                         <p>description</p>
+        //                     </div>
+        //                 </div> 
+        //             </div>
+        //         </div>
+        // </section>
     )
 }
 
