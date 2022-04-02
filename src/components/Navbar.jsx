4cd1d55a84ee61af66f37c6948a1af3608fffec3
Derek Lee <dyklee89@gmail.com>
2022-04-02 12:18:07 -0400

    update duration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ function Nav0() {
                         <h5 className='nav-home px-2' >Home</h5>
                     </Link>
                     
-                    <ScrollLink to='profile' spy={true} smooth={true} offset={-50} duration={50} style={{ color: 'rgb(255, 249, 215)', textDecoration: 'none'}}>
+                    <ScrollLink to='profile' spy={true} smooth={true} offset={-50} duration={400} style={{ color: 'rgb(255, 249, 215)', textDecoration: 'none'}}>
                         <h5 className='nav-about px-2'>About</h5>
                     </ScrollLink>
                  
-                    <ScrollLink to='projects' spy={true} smooth={true} offset={-50} duration={50} style={{ color: 'rgb(255, 249, 215)', textDecoration: 'none'}}>
+                    <ScrollLink to='projects' spy={true} smooth={true} offset={-50} duration={400} style={{ color: 'rgb(255, 249, 215)', textDecoration: 'none'}}>
                         <h5 className='nav-projects px-2'>Projects</h5>
                     </ScrollLink>
 
